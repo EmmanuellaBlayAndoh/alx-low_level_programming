dd1ecba4ff02dd4b1000785c0984aabf83e14743
EmmanuellaBlayAndoh <emmanuellablayandoh@gmail.com>
2022-10-18 13:59:23 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _abs - prints absolute number
+ * @ab: number input
+ *
+ * Return: the absolute vale of a
+ */
+int _abs(int ab)
+{
+	return (ab * ((ab > 0) - (ab < 0)));
+}
