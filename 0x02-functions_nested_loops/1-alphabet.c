86d19f5f23ec435e271d7bee19a683d79cb26b43
EmmanuellaBlayAndoh <emmanuellablayandoh@gmail.com>
2022-10-18 11:59:06 -0700

    alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,21 @@
 #include "main.h"
 
 /**
- * main - print alphabet
+ * print_alphabet - print alphabet
  *
  * Return: Always 0
  *
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	int i;
 
 	for (i = 'a'; i <= 'z'; i++)
+	{
 		_putchar(i);
+	}
 
 	_putchar('\n');
 
-	return (0);
+	return;
 }
