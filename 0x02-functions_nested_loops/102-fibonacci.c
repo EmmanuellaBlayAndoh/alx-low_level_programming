7216a0fa088dc0fa1cdd1f191f581a970c6f5080
EmmanuellaBlayAndoh <emmanuellablayandoh@gmail.com>
2022-10-18 18:26:12 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i, first = 0, second = 1, next;
+
+	for (i = 0; i <= 50; i++)
+	{
+		if (i <= 1)
+			next = i;
+		else
+		{
+			next = first + second;
+			first = second;
+			second = next;
+		}
+		printf("%d\n", next);
+	}
+
+	return (0);
+}
