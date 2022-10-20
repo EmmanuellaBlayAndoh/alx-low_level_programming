e9bd3189afcb6eb66b279eb5a6bb8252876916b4
EmmanuellaBlayAndoh <emmanuellablayandoh@gmail.com>
2022-10-20 15:04:49 -0700

    multiple

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,22 +1,17 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
+#include "main.h"
+
 
 /**
  * main - printing number
  * Return: Always 0
  */
-int positive_or_negative(void)
+
+void positive_or_negative(int i)
 {
-
-	int i;
-
 	if (i > 0)
 		printf("%d is positive\n", i);
 	else if (i == 0)
 		printf("%d is zero\n", i);
 	else
 		printf("%d is negative\n", i);
-
-	return (0);
 }
