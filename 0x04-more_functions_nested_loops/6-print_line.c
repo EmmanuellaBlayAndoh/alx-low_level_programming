a6cedd14bd27ecc6980e36a85e7cbea6744b97f2
EmmanuellaBlayAndoh <emmanuellablayandoh@gmail.com>
2022-10-21 16:47:01 -0700

    print lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,16 +8,16 @@
  */
 void print_line(int n)
 {
-	int i = 0;
+	int a = 0;
 
-	if (i > 0)
+	if (n > 0)
 	{
-		for (i = 1; i < n; i++)
+		while (a <= n)
 		{
 			_putchar('_');
-			_putchar('\n');
-
+			a++;
 		}
+		_putchar('\n');
 	}
 	else
 	{
