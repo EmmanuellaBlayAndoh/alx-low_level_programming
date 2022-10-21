ddbaf9a2afb5109c4198cb36e7156238b78819dc
EmmanuellaBlayAndoh <emmanuellablayandoh@gmail.com>
2022-10-21 15:17:45 -0700

    print lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,19 +3,18 @@
 /**
  * print_line - draws a straight line
  * @n:  number of times the character _ should be printed
- * 
+ *
  * Return: Always 0
  */
 void print_line(int n)
 {
-	int a = 0;
+	int i;
 
 	if (n > 0)
 	{
-		while (a <= n)
+		for (i = 1; i < n ; i++)
 		{
 			_putchar ('_');
-			a++;
 		}
 		_putchar ('\n');
 	}
---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal line
+ * @n: single input
+ * Return: Always 0
+ */
+void print_diagonal(int n)
+{
+	int a = 0;
+
+}
